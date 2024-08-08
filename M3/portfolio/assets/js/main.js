387ac01dd28bcac6a4e89bef6446b65f189bc54a
Arnold Vasquez <contacto@disroot.org>
2024-08-07 20:41:46 -0400

    finalizing minimum requirements for portfolio

--- M3/portfolio/assets/js/main.js
+++ M3/portfolio/assets/js/main.js
@@ -1,1 +1,24 @@
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+const links = document.querySelectorAll("nav a");
+
+links.forEach((link) => {
+	link.addEventListener("click", (e) => {
+		e.preventDefault();
+		const targetId = link.getAttribute("href").substring(1);
+		const targetElement = document.getElementById(targetId);
+
+		// Calcula el desplazamiento considerando la altura del navbar
+		const navbarHeight = document.querySelector("nav").offsetHeight;
+		const targetPosition = targetElement.offsetTop - navbarHeight;
+
+		window.scrollTo({
+			top: targetPosition,
+			behavior: "smooth",
+		});
+	});
+});
+
+// const scrollSpy = new bootstrap.ScrollSpy(document.body, {
+//   target: 'nav'
+// })
